aa9c9799f350fa8c083b3b665370bcef7ef6c201
Saqib <isaqib23@gmail.com>
2019-10-21 16:40:10 +0500

    Influencer Settings Done

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -5,23 +5,5 @@
 </template>
 
 <script>
-    import { mapGetters } from 'vuex'
-export default {
-    computed: mapGetters({
-        auth: 'auth/user'
-    }),
-    mounted() {
-        console.log(this.auth, this.$store.state.auth);
-        //console.log(this.$store.state.auth);
-        if(this.$store.state.auth){
-            if(this.$store.state.auth.user.type == 'influencer'){
-                this.$router.push({ name: 'influencer-index' })
-            }else{
-                this.$router.push({ name: 'businessOwner-index' })
-            }
-        }else{
-            this.$router.push({ name: 'login' })
-        }
-    }
-}
+export default {}
 </script>
