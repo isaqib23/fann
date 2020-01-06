077980f0d685bfd47a5504a1664e6b7c88660acd
Saqib <isaqib23@gmail.com>
2020-01-06 10:53:06 +0500

    PR Changes done

--- resources/js/apiRoutes/campaign-management.js
+++
@@ -1,9 +0,0 @@
-let campaign = {
-    url: 'campaign_manage',
-
-    getActiveCampaigns  : 'getActiveCampaigns',
-    getCampaignById     : 'getCampaignById',
-    getCampaignProposal : 'getCampaignProposal'
-};
-
-export default campaign;
